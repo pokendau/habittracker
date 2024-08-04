52b25e7dbe2ade7a0bf65b59833048b3338e3468
daniel <daniplingau@gmail.com>
2024-08-04 09:01:16 +0300

    Added firestore

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { browserLocalPersistence, getAuth, setPersistence } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { API_KEY, APP_ID, PROJECT_ID } from "$env/static/private";
 
 const firebaseConfig = {
@@ -14,6 +15,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+export const db = getFirestore(app)
 
 setPersistence(auth, browserLocalPersistence)
   .catch((error) => {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>This is the habit tracker you've been loogking for.</h1>
-<h2>Go and <a href="/authentication">log</a> in to access all of the features!</h2>
+<h2>Go and <a href="/auth/login">log</a> in to access all of the features!</h2>
