c8c2172b0a9e77cde89e28351de9852eef95d545
daniel <daniplingau@gmail.com>
2024-08-06 12:36:42 +0300

    The small boxes in dashboard

---
+++ src/lib/components/Tracker.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let name = "Unknown";
+  export let link ="/";
+</script>
+
+<a href={link} class="w-full flex aspect-square border-2 border-black rounded-lg items-center justify-center hover:bg-gray-100 duration-200 font-bold text-2xl text-center">
+  {name}
+</a>
